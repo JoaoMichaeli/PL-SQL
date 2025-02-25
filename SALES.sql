3a41af3d49c172c785d4cb0326f64fa1e8a784b6
JoaoFIAP <joaovictor_de_bem_@hotmail.com>
2025-02-25 11:42:51 -0300

    add sql

---
+++ SALES.sql
@@ -0,0 +1,15 @@
+SELECT * FROM sales;
+
+-- Criar um bloco para trazer a quantidade de pedidos por país
+
+DECLARE
+    v_pais VARCHAR2(50) := '&PAIS';
+    v_pedidos NUMBER;
+    v_faturamento NUMBER;
+BEGIN
+    SELECT COUNT(quantityordered) INTO v_pedidos FROM sales WHERE country = (v_pais);
+    SELECT SUM(priceeach) INTO v_faturamento FROM sales WHERE country = (v_pais);
+    dbms_output.put_line('País selecionado: ' || v_pais);
+    dbms_output.put_line('Quantidade de pedidos: ' || v_pedidos);
+    dbms_output.put_line('Faturamento: ' || v_faturamento);
+END;
